ddde273c3104d0ed8c3781c0b47ea98be177133e
Yusuke Hosonuma <tobi462@gmail.com>
2018-06-17 16:07:22 +0900

    update: ssh256 key

--- nene.rb
+++ nene.rb
@@ -3,7 +3,7 @@ require "formula"
 class Nene < Formula
   homepage "https://github.com/YusukeHosonuma/nene"
   url "https://github.com/YusukeHosonuma/nene/releases/download/0.1.0/nene.zip"
-  sha256 "8e96d3afe4792cf79f3e1f82a51eeafe68373349f4cec7bc33b070e5f5a99eee"
+  sha256 "12ec4edd38f1bdb78356adb90d3fd43105bbb01522cf7fe7989fe88bb82a0e83"
   version "0.1.0"
 
   def install
