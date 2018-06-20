e487a4334f8c4e7442c5b3a0b6b979ac7db42332
Yusuke Hosonuma <tobi462@gmail.com>
2018-06-21 01:05:28 +0900

    Update version to 0.1.1

--- nene.rb
+++ nene.rb
@@ -2,9 +2,9 @@ require "formula"
 
 class Nene < Formula
   homepage "https://github.com/YusukeHosonuma/nene"
-  url "https://github.com/YusukeHosonuma/nene/releases/download/0.1.0/nene.zip"
-  sha256 "12ec4edd38f1bdb78356adb90d3fd43105bbb01522cf7fe7989fe88bb82a0e83"
-  version "0.1.0"
+  url "https://github.com/YusukeHosonuma/nene/releases/download/0.1.1/nene.zip"
+  sha256 "14f873c564f4bd265e37affd360570918055c28c516f179bf9f9aff8c5684706"
+  version "0.1.1"
 
   def install
     bin.install "nene"
